5bd6c83d202d90adb1249b2795621bae135e69e7
Emily-Rose Steyn <emilyrosesteyn@gmail.com>
2024-07-27 13:40:58 -0400

    Fixing model and table saving to have expected data

--- src/valuations/services/valuation_table_utils.py
+++ src/valuations/services/valuation_table_utils.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import pandas as pd
 
 from django.conf import settings
@@ -37,7 +39,7 @@ def save_table_data(table, roll_type):
     df = pd.DataFrame(data, columns=headers)
 
     if not df.empty:
-        print('Saving data')
+        # Rename columns
         df = df.rename(columns={
             'Rate Number': 'rate_number',
             'Legal Description': 'legal_description',
@@ -50,9 +52,17 @@ def save_table_data(table, roll_type):
         })
         df["roll_type"] = roll_type
 
-        # TODO: Actually save the data
-        print(df)
-        df.to_sql('valuations_valuation', engine, if_exists='append', index=False)
+        # Index and order for consistency
+        df.set_index('rate_number', inplace=True)
+        df.sort_index(inplace=True)
+
+        # Modify types
+        df['market_value'] = df['market_value'].str.replace(',', '').astype(float)
+        df['registered_extent'] = df['registered_extent'].str.replace(',', '').astype(float)
+
+        # Save the data
+        print('Saving data')
+        df.to_sql('valuations_valuation', engine, if_exists='append', index=True)
 
     else:
         print('No data found')
